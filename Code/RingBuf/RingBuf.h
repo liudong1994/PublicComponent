92770e252a7782c59be61df383ad18c9c43ec0e2
DESKTOP-K39LSSD\ld <276340427@qq.com>
2017-12-24 09:41:16 +0800

    添加对ringbuf的使用方法代码

---
+++ Code/RingBuf/RingBuf.h
@@ -0,0 +1,40 @@
+#ifndef _TCP_RING_H_
+#define _TCP_RING_H_
+
+
+#define MAX_RING_SIZE		1024 * 1024 * 500
+
+
+class CRingBuf
+{
+public:
+	CRingBuf();
+	~CRingBuf();
+
+public:
+	//初始化
+	bool Init(unsigned int nSize = MAX_RING_SIZE);
+
+    void Clear();
+
+	//写入数据
+	char *Write(char *pData, unsigned int nLen);
+
+	//设置读位置
+	bool SetRead(char *pData);
+
+	float GetUsed(unsigned int& uDrop);
+
+public:
+	char *m_pData;
+
+	unsigned int m_nWrPos;		//写指针
+	unsigned int m_nRdPos;		//读指针
+
+	unsigned int m_nSize;
+
+	unsigned int m_uDropCount;
+	
+};
+#endif
+
---
+++ Code/RingBuf/main.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "RingBuffer.h"
+#include "RingBuf.h"
+
+
+struct TestNode
+{
+    char    *pData;
+    int     nLen;
+
+    TestNode(): pData(NULL), nLen(0)
+    {
+    }
+};
+
+int main()
+{
+    CRingBuf							ringData;
+    CRingBuffer<TestNode, 300>		    nodeRing;
+
+    ringData.Init(102400);
+    nodeRing.SetBufferName("Test Node Ring");
+
+
+    char bufName[1024];
+    while(gets(bufName) != NULL && 0 != strcmp(bufName, "end"))
+    {
+        printf("input: %s\n", bufName);
+
+        TestNode node;
+        node.nLen = strlen(bufName);
+        node.pData = ringData.Write(bufName, node.nLen);
+
+        if (NULL != node.pData)
+        {
+            nodeRing.Push(node);
+        }
+        else
+        {
+            printf("node ring add failed\n");
+        }
+    }
+
+    printf("--------------------------------------\n");
+
+    TestNode node;
+    while (nodeRing.Pop(node))
+    {
+        printf("just input(%d)->%s\n", node.nLen, node.pData);
+        ringData.SetRead(node.pData);
+    }
+
+    return 0;
+}
+
